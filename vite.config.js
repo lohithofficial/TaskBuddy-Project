8b9b164aa8c27728cbc8b6a8ffe7ec3df79f8e20
lohithofficial <lohith2141@gmail.com>
2025-10-06 01:08:57 +0530

    Fix vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TaskBuddy-Project/', // 👈 Add this line
+  base: '/TaskBuddy-Project/', // 👈 Correctly placed
 })
